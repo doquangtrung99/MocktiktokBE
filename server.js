b5b280590197cc7d22edfe8ddc3d3bf6b78d5832
trungdq1 <trungdq1@smartosc.com>
2023-06-30 01:06:43 +0700

    test

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ mongoose.set('strictQuery', false);
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
+    origin: '*',
 }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
